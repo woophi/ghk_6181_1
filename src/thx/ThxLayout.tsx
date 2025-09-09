bc7fe0c0f033473c1536b678fe9ee7b69113b2f5
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-09-09 12:29:50 +0200

    fix

--- src/thx/ThxLayout.tsx
+++ src/thx/ThxLayout.tsx
@@ -1,27 +1,26 @@
 import { ButtonMobile } from '@alfalab/core-components/button/mobile';
 import { Typography } from '@alfalab/core-components/typography';
-import sparkles from '../assets/sparkles.png';
+import sparkles from '../assets/rocket.png';
 import { appSt } from '../style.css';
 import { thxSt } from './style.css';
 
-const link =
-  'alfabank://investments/market_asset_card?title=%D0%9D%D0%9E%D0%92%D0%90%D0%A2%D0%AD%D0%9A&id=RU000A0DKVS5&subtitle=NVTK&url=v1%2Fscreen-details-prefiller%2Fproducts%2Fbrokerage%2FSTOCK%2FRU000A0DKVS5&type=STOCK';
+const link = 'alfabank:///';
 
 export const ThxLayout = () => {
   return (
     <>
       <div className={thxSt.container}>
-        <img src={sparkles} width={80} height={80} className={thxSt.rocket} />
-        <Typography.TitleResponsive style={{ margin: '24px 0 12px' }} font="system" tag="h1" view="small" weight="medium">
-          Не удалось выставить заявку
+        <img src={sparkles} width={280} height={280} className={thxSt.rocket} />
+        <Typography.TitleResponsive style={{ margin: '24px 0 12px' }} font="system" tag="h1" view="medium" weight="medium">
+          Спасибо за участие
         </Typography.TitleResponsive>
         <Typography.Text tag="p" view="primary-medium" defaultMargins={false}>
-          Попробуй еще раз
+          Мы проводим исследование для нового сервиса. Скоро расскажем о нём подробнее, следите за новостями!
         </Typography.Text>
       </div>
       <div className={appSt.bottomBtn}>
         <ButtonMobile block view="secondary" href={link}>
-          Попробовать еще раз
+          На главный
         </ButtonMobile>
       </div>
     </>
